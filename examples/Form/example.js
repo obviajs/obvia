089e9ae020bf1acae6125825720145ca6965c716
mateo <mateojovani@yahoo.com>
2018-02-16 16:00:09 +0100

    Autocomplete fix

--- examples/Form/example.js
+++ examples/Form/example.js
@@ -19,7 +19,7 @@ var myForm = new Form({
             label: 'Ministrite',
             blockProcessAttr: false,
             required: false,
-            multipleSelection: true,
+            multipleSelection: false,
             tableData: [["Ministria e Puneve te Jashtme"], ["Ministria e Drejtesise"], ["Ministria e Brendshme"]],
             dataProvider: [{ "id": "1", "text": "Ministria e Puneve te Jashtme" }, { "id": "2", "text": "Ministria e Drejtesise" }, { "id": "3", "text": "Ministria e Brendshme" }],
             value: [{ "id": "1", "text": "Ministria e Puneve te Jashtme" }]
@@ -156,7 +156,6 @@ var myForm = new Form({
                         id: 'autocompleteR',
                         colspan: '6',
                         label: '{autocompleteLabel}',
-                        fieldName: 'autocompleteR',
                         blockProcessAttr: false,
                         required: false,
                         multipleSelection: true,
