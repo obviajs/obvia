89d900e0e43d16a7e9a7d1ebc66a0857562a201c
mateo <mateojovani@yahoo.com>
2018-02-21 17:44:35 +0100

    Label Component

--- examples/Label/example.js
+++ examples/Label/example.js
@@ -1,8 +1,6 @@
 var myLabel = new Label({
     id: 'label',
-    label: 'Button Label',
-    class:"form-group col-lg-12 rowspan6 resizable",
-    style:'margin-top: 15px;background-color: #CAC8C8; font-size: 16px;'
+    label: 'Label'
 });
 
 $('#root').append(myLabel.render());
--- js/components/Label.js
+++ js/components/Label.js
@@ -1,41 +1,46 @@
 /**
- * This is a Text Input Element
+ * This is a Label Element
  * 
  * Kreatx 2018
  */
 
 //component definition
 var Label = KxGenerator.createComponent({
-    //model binds to the template
-    //if you want variables to bind, you must declare them in the model object
+    //component data
     initModel: function () {
         return {
-            label: this.label,
-            class:this.class,
-            style:this.style
+            class: "form-group col-lg-12 resizable",
+            style: 'margin-top: 15px; background-color: #CAC8C8; font-size: 16px;'
         }
     },
 
-    beforeAttach: function () { //function that is executed before you render the  component in the DOM
-        //life0-cycle method //futet ne DOM qe te besh inicimet e plugineve
-        
+    registerEvents: function () {
+        this.$label = this.$el.find("label");
+
+        return [
+            {
+                registerTo: this.$el, events: {
+                    'afterAttach': this.afterAttach.bind(this)
+                }
+            }
+        ]
     },
 
-    afterAttach: function () {
-
+    afterAttach: function (e) {
+        this.trigger('creationComplete');
     },
 
     getValue: function () {
-
+        return null;
     },
 
     setValue: function (value) {
-
+        return null
     },
 
     template: function () {         
-        return  "<div id="+this.domID+" rv-class='class' rv-style='style'>"+
-                    "<label>{label} </label>"+
+        return  "<div id='" + this.domID + "-wrapper' rv-class='model.class' rv-style='model.style'>"+
+                    "<label>{label}</label>"+
                 "</div>";    
     },
 
