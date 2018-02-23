7d5c45c15f379d33020f8116740c683e3717747d
mateo <mateojovani@yahoo.com>
2018-02-23 14:33:04 +0100

    Atucomplete displayTable property for more flexible view mode

--- examples/Form/example.js
+++ examples/Form/example.js
@@ -104,6 +104,7 @@ var myForm = new Form({
                 blockProcessAttr: false,
                 required: false,
                 multipleSelection: false,
+                displayTable: false,
                 tableData: [["Ministria e Puneve te Jashtme"], ["Ministria e Drejtesise"], ["Ministria e Brendshme"]],
                 dataProvider: [{ "id": "1", "text": "Ministria e Puneve te Jashtme" }, { "id": "2", "text": "Ministria e Drejtesise" }, { "id": "3", "text": "Ministria e Brendshme" }],
                 value: [{ "id": "1", "text": "Ministria e Puneve te Jashtme" }]
@@ -319,6 +320,7 @@ var myForm = new Form({
                             blockProcessAttr: false,
                             required: false,
                             multipleSelection: true,
+                            displayTable: true,
                             tableData: [["Ministria e Puneve te Jashtme"], ["Ministria e Drejtesise"], ["Ministria e Brendshme"]],
                             dataProvider: [{ "id": "1", "text": "Ministria e Puneve te Jashtme" }, { "id": "2", "text": "Ministria e Drejtesise" }, { "id": "3", "text": "Ministria e Brendshme" }],
                             value: '{autocompleteValue}'
