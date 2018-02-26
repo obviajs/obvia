da8ed6af403bcf1353ab0e2356e1323a4ff854fe
lejdi <lejdi.koci@gmail.com>
2018-02-26 15:06:32 +0100

    added evalBindingExpression

--- examples/List/example.js
+++ examples/List/example.js
@@ -8,15 +8,15 @@ var list = new List({
     direction: 'horizontal',
     multiselect: this.multiselect,
     dataProvider: [
-        { "id": "1", "text": "Ministria e Puneve te Jashtme 1", "buttonClass": 'btn btn-xs btn-default'},
-        { "id": "2", "text": "Ministria e Drejtesise 1", "buttonClass": 'btn btn-xs btn-default'},
-        { "id": "3", "text": "Ministria e Brendshme 1", "buttonClass": 'btn btn-xs btn-default'},
-        { "id": "4", "text": "Ministria e Mbrojtjes 1", "buttonClass": 'btn btn-xs btn-default'}
+        { "id": "1", "text": "Ministria e Puneve te Jashtme 1", "buttonClass": 'btn btn-sm btn-default'},
+        { "id": "2", "text": "Ministria e Drejtesise 1", "buttonClass": 'btn btn-sm btn-default'},
+        { "id": "3", "text": "Ministria e Brendshme 1", "buttonClass": 'btn btn-sm btn-default'},
+        { "id": "4", "text": "Ministria e Mbrojtjes 1", "buttonClass": 'btn btn-sm btn-default'}
     ],
     valueField: "id",
     classField: "buttonClass",
-    defaultClass: "btn btn-xs btn-default",
-    selectedClass: "btn btn-xs btn-success",    
+    defaultClass: "btn btn-sm btn-default",
+    selectedClass: "btn btn-sm btn-success",    
     value:[{ "id": "1", "text": "Ministria e Puneve te Jashtme 1", "buttonClass": 'btn btn-xs btn-default'}],      
     components: [
         {
@@ -44,15 +44,15 @@ var list2 = new List({
     direction: 'horizontal',
     multiselect: this.multiselect,
     dataProvider: [
-        { "id": "1", "text": "Ministria e Puneve te Jashtme 1", "buttonClass": 'btn btn-xs btn-default'},
-        { "id": "2", "text": "Ministria e Drejtesise 1", "buttonClass": 'btn btn-xs btn-default'},
-        { "id": "3", "text": "Ministria e Brendshme 1", "buttonClass": 'btn btn-xs btn-default'},
-        { "id": "4", "text": "Ministria e Mbrojtjes 1", "buttonClass": 'btn btn-xs btn-default'}
+        { "id": "1", "text": "Ministria e Puneve te Jashtme 1", "buttonClass": 'btn btn-sm btn-default'},
+        { "id": "2", "text": "Ministria e Drejtesise 1", "buttonClass": 'btn btn-sm btn-default'},
+        { "id": "3", "text": "Ministria e Brendshme 1", "buttonClass": 'btn btn-sm btn-default'},
+        { "id": "4", "text": "Ministria e Mbrojtjes 1", "buttonClass": 'btn btn-sm btn-default'}
     ],
     valueField: "id",
     classField: "buttonClass",
-    defaultClass: "btn btn-xs btn-default",
-    selectedClass: "btn btn-xs btn-success",    
+    defaultClass: "btn btn-sm btn-default",
+    selectedClass: "btn btn-sm btn-success",    
     value:[{ "id": "1", "text": "Ministria e Puneve te Jashtme 1", "buttonClass": 'btn btn-xs btn-default'}],      
     components: [
         {
