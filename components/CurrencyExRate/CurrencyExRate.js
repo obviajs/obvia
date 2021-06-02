6b1c289b3b60d2619433f4b3ce6bda40bad9c8fb
Xhoni <Xhonikomini96@gmail.com>
2021-06-02 11:07:39 +0200

    Autobrowse & CurrencyExRate Container type removed . new Loader.js

--- components/CurrencyExRate/CurrencyExRate.js
+++ components/CurrencyExRate/CurrencyExRate.js
@@ -6,111 +6,107 @@
 
 import { Container } from "/flowerui/components/Container.js";
 import { ObjectUtils } from "/flowerui/lib/ObjectUtils.js";
-import { ContainerType } from "/flowerui/components/base/ContainerType.js";
-
+import { DropDown } from "../DropDown/DropDown.js";
+import { TextInput } from "../TextInput/TextInput.js";
 var CurrencyExRate = function (_props) {
-    let _self = this;
+	let _self = this;
 
-    Object.defineProperty(this, "value", {
-        get: function value() {
-            return _value;
-        },
-        set: function value(v) {
-            if (JSON.stringify(_value) != JSON.stringify(v)) {
-                _value.exRate = v.exRate;
-                _value.currency = v.currency;
+	Object.defineProperty(this, "value", {
+		get: function value() {
+			return _value;
+		},
+		set: function value(v) {
+			if (JSON.stringify(_value) != JSON.stringify(v)) {
+				_value.exRate = v.exRate;
+				_value.currency = v.currency;
 
-                this.workArea_53.exchangeRate.value = v.exRate;
-                this.workArea_53.currencySelect.selectedItem.currency_id = v.currency;
-            }
-        },
-        enumerable: true,
-    });
+				this.workArea_53.exchangeRate.value = v.exRate;
+				this.workArea_53.currencySelect.selectedItem.currency_id = v.currency;
+			}
+		},
+		enumerable: true,
+	});
 
-    this.changeHandler = function (e) {
-        _value.exRate = this.workArea_53.exchangeRate.value;
-        _value.currency = this.workArea_53.currencySelect.selectedItem.currency_id;
-    };
-    this.afterAttach = function (e) {
-        if (e.target.id == this.domID) {
+	this.changeHandler = function (e) {
+		_value.exRate = this.workArea_53.exchangeRate.value;
+		_value.currency = this.workArea_53.currencySelect.selectedItem.currency_id;
+	};
+	this.afterAttach = function (e) {
+		if (e.target.id == this.domID) {
+		}
+	};
+	let _cmps;
+	let fnContainerDelayInit = function () {
+		_cmps = [
+			{
+				ctor: "Container",
+				props: {
+					type: "",
+					id: "workArea_53",
+					css: {
+						display: "flex",
+						width: "100%",
+					},
+					components: [
+						{
+							ctor: DropDown,
+							props: {
+								id: "currencySelect",
+								dataProvider: _currencyList,
+								labelField: _labelField,
+								valueField: _valueField,
+								value: _value.currency,
+								css: {
+									float: "left",
+								},
+							},
+						},
+						{
+							ctor: TextInput,
+							props: {
+								id: "exchangeRate",
+								value: _value.exRate,
+								css: {},
+							},
+						},
+					],
+				},
+			},
+		];
+	};
 
-        }
-    };
-    let _cmps;
-    let fnContainerDelayInit = function () {
-        _cmps = [
+	let _defaultParams = {
+		value: {
+			exRate: "",
+			currency: "1",
+		},
+		classes: ["d-flex"],
+		type: "",
+		currencyList: [],
+		labelField: "title",
+		valueField: "key",
+	};
+	_props = ObjectUtils.extend(false, false, _defaultParams, _props);
 
-            {
-                "ctor": "Container",
-                "props": {
-                    type: ContainerType.NONE,
-                    "id": "workArea_53",
-                    css: {
-                        "display": "flex",
-                        "width": "100%"
-                    },
-                    "components": [
-                        {
-                            ctor: DropDown,
-                            props: {
-                                id: "currencySelect",
-                                dataProvider: _currencyList,
-                                labelField: _labelField,
-                                valueField: _valueField,
-                                value: _value.currency,
-                                css: {
-                                    float: 'left'
-                                }
-                            }
-                        },
-                        {
-                            ctor: TextInput,
-                            props: {
-                                id: "exchangeRate",
-                                value: _value.exRate,
-                                css: {}
-                            }
-                        }
-                    ]
-                }
-            }
-        ];
-    };
+	let _value = _props.value;
+	let _currencyList = _props.currencyList;
+	let _labelField = _props.labelField;
+	let _valueField = _props.valueField;
+	let _change = _props.change;
 
+	_props.change = function () {
+		if (typeof _change == "function") _change.apply(this, arguments);
 
-    let _defaultParams = {
-        value: {
-            exRate: "",
-            currency: "1"
-        },
-        classes: ["d-flex"],
-        type: ContainerType.NONE,
-        currencyList: [],
-        labelField: "title",
-        valueField: "key"
-    };
-    _props = ObjectUtils.extend(false, false, _defaultParams, _props);
-
-    let _value = _props.value;
-    let _currencyList = _props.currencyList;
-    let _labelField = _props.labelField;
-    let _valueField = _props.valueField;
-    let _change = _props.change;
-
-
-    _props.change = function () {
-        if (typeof _change == 'function')
-            _change.apply(this, arguments);
-
-        let e = arguments[0];
-        if (!e.isDefaultPrevented()) {
-            _self.changeHandler();
-        }
-    };
-    fnContainerDelayInit();
-    _props.components = _cmps;
-    let r = Container.call(this, _props, true);
-    return r;
+		let e = arguments[0];
+		if (!e.isDefaultPrevented()) {
+			_self.changeHandler();
+		}
+	};
+	fnContainerDelayInit();
+	_props.components = _cmps;
+	let r = Container.call(this, _props, true);
+	return r;
 };
 //component prototype
-CurrencyExRate.prototype.ctor = 'CurrencyExRate';
+CurrencyExRate.prototype.ctor = "CurrencyExRate";
+export { CurrencyExRate };
--- components/Loader.js
+++ components/Loader.js
@@ -1,42 +1,55 @@
 /**
  * This is a Loader Element
- * 
+ *
  * Kreatx 2018
  */
 
+// import { Env } from "../lib/Env.js";
 import { Component } from "/flowerui/components/base/Component.js";
 import { ObjectUtils } from "/flowerui/lib/ObjectUtils.js";
-var Loader = function(_props)
-{ 
-    this.template = function () 
-    {
-        return  "<div id='" + this.domID + "'>" +
-                    "<style>" +
-                        ".se-pre-con {" +
-                            "position: fixed;" +
-                            "left: 0px;" +
-                            "top: 0px;" +
-                            "width: 100%;" +
-                            "height: 100%;" +
-                            "z-index: 999999;" +
-                            "opacity: 0.9;" +
-                            "background: url('"+(Env.getInstance().baseurl)+"/flowerui/lib/dependencies/images/loader.gif')center no-repeat #fff;" +
-                        "}" +
-                    "</style>"+
-                    "<div class='se-pre-con'></div>" +                    
-                "</div>";
-    };
-    var _defaultParams = {
-        visible: true
-    };
+var Loader = function (_props) {
+	this.template = function () {
+		return `
+		<div class="spinner">
+		<svg class="svg_spinner">
+		  <use href="/flowerui/assets/icons.svg#icon-loader"></use>
+		</svg>
+	  </div>
+		`;
+	};
 
-    _props = ObjectUtils.extend(false, false, _defaultParams, _props);
+	// this.template = function () {
+	// 	return (
+	// 		"<div id='" +
+	// 		this.domID +
+	// 		"'>" +
+	// 		"<style>" +
+	// 		".se-pre-con {" +
+	// 		"position: fixed;" +
+	// 		"left: 0px;" +
+	// 		"top: 0px;" +
+	// 		"width: 100%;" +
+	// 		"height: 100%;" +
+	// 		"z-index: 999999;" +
+	// 		"opacity: 0.9;" +
+	// 		"background: url('" +
+	// 		Env.getInstance().baseurl +
+	// 		"/flowerui/lib/dependencies/images/loader.gif')center no-repeat #fff;" +
+	// 		"}" +
+	// 		"</style>" +
+	// 		"<div class='se-pre-con'></div>" +
+	// 		"</div>"
+	// 	);
+	// };
+	var _defaultParams = {
+		visible: true,
+	};
 
-    Component.call(this, _props);
+	_props = ObjectUtils.extend(false, false, _defaultParams, _props);
+
+	Component.call(this, _props);
 };
 
 //component prototype
-Loader.prototype.ctor = 'Loader';
-export {
-    Loader
-};
+Loader.prototype.ctor = "Loader";
+export { Loader };
