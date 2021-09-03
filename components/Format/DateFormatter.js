8a9da46c30fa6deb1e60312f894100e676e86219
Lejdi Koci <lejdi.koci@kreatx.com>
2021-09-03 15:08:16 +0200

    Added NumberFormatter.js, DateFormatter.js

---
+++ components/Format/DateFormatter.js
@@ -0,0 +1,58 @@
+/**
+ * This is a DateFormatter Element, the default item renderer for a DataGrid
+ * 
+ * Kreatx 2021
+ */
+
+import { Label } from "/obvia/components/Label.js";
+import { DependencyContainer } from "/obvia/lib/DependencyContainer.js";
+import { DateUtils } from "/obvia/lib/DateUtils.js";
+
+var DateFormatter = function (_props) {
+    //component data
+    let _label, _inputFormat, _outputFormat;
+    let _defaultParams = {
+        inputFormat: 'YYYY-MM-DDTHH:mm',
+        outputFormat: 'DD/MM/YYYY HH:mm',
+    };
+    _inputFormat = _props.inputFormat;
+    _outputFormat = _props.outputFormat;
+    
+    ObjectUtils.fromDefault(_defaultParams, _props);
+
+    let r = Label.call(this, _props);
+    Object.defineProperty(this, "label", {
+        get: function label() {
+            return _label;
+        },
+        set: function label(v) {
+            if (_label != v) {
+                _props.label = _label = v;
+                if (this.$el) {
+                    //convert html entities
+                    v = $(`<div>${v}</div>`).get(0).innerText;
+                    let last = this.$el.children().last();
+                    v = DateUtils.format(v, _inputFormat, _outputFormat);
+                    if (last && last.length > 0)
+                        if (last[0].nextSibling)
+                            last[0].nextSibling.textContent = v;
+                        else
+                            this.$el.appendText(v);
+                    else
+                        //this.$el.appendText(v);
+                        this.$el.text(v);
+                }
+            }
+        },
+        enumerable: true,
+        configurable: true
+    });
+    return r;
+};
+ 
+//component prototype
+DateFormatter.prototype.ctor = 'DateFormatter';
+DependencyContainer.getInstance().register("DateFormatter", DateFormatter, DependencyContainer.simpleResolve);
+export {
+    DateFormatter
+};
---
+++ components/Format/NumberFormatter.js
@@ -0,0 +1,55 @@
+/**
+ * This is a NumberFormatter Element, the default item renderer for a DataGrid
+ * 
+ * Kreatx 2021
+ */
+
+import { Label } from "/obvia/components/Label.js";
+import { DependencyContainer } from "/obvia/lib/DependencyContainer.js";
+import { NumberUtils } from "/obvia/lib/NumberUtils.js";
+var NumberFormatter = function (_props) {
+    //component data
+    let _label, _precision;
+    let _defaultParams = {
+        precision: 2
+    };
+
+    ObjectUtils.fromDefault(_defaultParams, _props);
+    _precision = _props.precision;
+
+    let r = Label.call(this, _props);
+    Object.defineProperty(this, "label", {
+        get: function label() {
+            return _label;
+        },
+        set: function label(v) {
+            if (_label != v) {
+                _props.label = _label = v;
+                if (this.$el) {
+                    //convert html entities
+                    v = $(`<div>${v}</div>`).get(0).innerText;
+                    let last = this.$el.children().last();
+                    v = NumberUtils.addCommas(NumberUtils.toFixed(v, _precision));
+                    if (last && last.length > 0)
+                        if (last[0].nextSibling)
+                            last[0].nextSibling.textContent = v;
+                        else
+                            this.$el.appendText(v);
+                    else
+                        //this.$el.appendText(v);
+                        this.$el.text(v);
+                }
+            }
+        },
+        enumerable: true,
+        configurable: true
+    });
+    return r;
+};
+ 
+//component prototype
+NumberFormatter.prototype.ctor = 'NumberFormatter';
+DependencyContainer.getInstance().register("NumberFormatter", NumberFormatter, DependencyContainer.simpleResolve);
+export {
+    NumberFormatter
+};
