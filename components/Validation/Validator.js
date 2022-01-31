75af1e64d24026e46b99a47bdbb6c15fc885d108
Lejdi Koci <lejdi.koci@kreatx.com>
2022-01-31 17:24:00 +0100

    Minor fix for errorMessage setter.

--- components/Validation/Validator.js
+++ components/Validation/Validator.js
@@ -8,7 +8,8 @@ import { Label, LabelType } from "/obvia/components/Label.js";
 import { ObjectUtils } from "/obvia/lib/ObjectUtils.js";
 import { ValidationManager } from "/obvia/components/Validation/ValidationManager.js";
 
-var Validator = function (_props) {
+var Validator = function (_props)
+{
     let _self = this,
         _isValid = true,
         _controlToValidate, _controlToValidateInstance,
@@ -16,21 +17,27 @@ var Validator = function (_props) {
         _invalidClasses, _validClasses, _required = false
 
     Object.defineProperty(this, "isValid", {
-        get: function isValid() {
+        get: function isValid()
+        {
             return _isValid;
         },
-        set: function isValid(v) {
-            if (_isValid != v) {
+        set: function isValid(v)
+        {
+            if (_isValid != v)
+            {
                 _isValid = v;
             }
-            if (!_isValid) {
+            if (!_isValid)
+            {
                 _self.label = _errorMessage;
                 _self.show();
-                if (_setFocusOnError && _controlToValidateInstance) {
+                if (_setFocusOnError && _controlToValidateInstance)
+                {
                     _controlToValidateInstance.focus();
                 }
                 _self.classes = _invalidClasses;
-            } else {
+            } else
+            {
                 _self.label = "";
                 _self.hide();
                 _self.classes = _validClasses;
@@ -38,13 +45,16 @@ var Validator = function (_props) {
         },
         enumerable: true
     });
-    
+
     Object.defineProperty(this, "required", {
-        get: function required() {
+        get: function required()
+        {
             return _required;
         },
-        set: function required(v) {
-            if (_required != v) {
+        set: function required(v)
+        {
+            if (_required != v)
+            {
                 _required = v;
             }
         },
@@ -53,11 +63,14 @@ var Validator = function (_props) {
     });
 
     Object.defineProperty(this, "validationGroup", {
-        get: function validationGroup() {
+        get: function validationGroup()
+        {
             return _validationGroup;
         },
-        set: function validationGroup(v) {
-            if (_validationGroup != v) {
+        set: function validationGroup(v)
+        {
+            if (_validationGroup != v)
+            {
                 _validationGroup = v;
             }
         },
@@ -66,11 +79,14 @@ var Validator = function (_props) {
     });
 
     Object.defineProperty(this, "setFocusOnError", {
-        get: function setFocusOnError() {
+        get: function setFocusOnError()
+        {
             return _setFocusOnError;
         },
-        set: function setFocusOnError(v) {
-            if (_setFocusOnError != v) {
+        set: function setFocusOnError(v)
+        {
+            if (_setFocusOnError != v)
+            {
                 _setFocusOnError = v;
             }
         },
@@ -79,12 +95,19 @@ var Validator = function (_props) {
     });
 
     Object.defineProperty(this, "errorMessage", {
-        get: function errorMessage() {
+        get: function errorMessage()
+        {
             return _errorMessage;
         },
-        set: function errorMessage(v) {
-            if (_errorMessage != v) {
+        set: function errorMessage(v)
+        {
+            if (_errorMessage != v)
+            {
                 _errorMessage = v;
+                if (!_isValid)
+                {
+                    _self.label = _errorMessage;
+                }
             }
         },
         enumerable: true,
@@ -92,11 +115,14 @@ var Validator = function (_props) {
     });
 
     Object.defineProperty(this, "controlToValidate", {
-        get: function controlToValidate() {
+        get: function controlToValidate()
+        {
             return _controlToValidate;
         },
-        set: function controlToValidate(v) {
-            if (_controlToValidate != v) {
+        set: function controlToValidate(v)
+        {
+            if (_controlToValidate != v)
+            {
                 _controlToValidate = v;
                 if (_controlToValidate && _self.parent)
                 {
@@ -116,11 +142,14 @@ var Validator = function (_props) {
     });
 
     Object.defineProperty(this, "setFocusOnError", {
-        get: function setFocusOnError() {
+        get: function setFocusOnError()
+        {
             return _setFocusOnError;
         },
-        set: function setFocusOnError(v) {
-            if (_setFocusOnError != v) {
+        set: function setFocusOnError(v)
+        {
+            if (_setFocusOnError != v)
+            {
                 _setFocusOnError = v;
             }
         },
@@ -129,13 +158,17 @@ var Validator = function (_props) {
     });
 
     Object.defineProperty(this, "controlToValidateInstance", {
-        get: function controlToValidateInstance() {
+        get: function controlToValidateInstance()
+        {
             return _controlToValidateInstance;
         },
-        set: function controlToValidateInstance(v) {
-            if (_controlToValidateInstance != v) {
+        set: function controlToValidateInstance(v)
+        {
+            if (_controlToValidateInstance != v)
+            {
                 _controlToValidateInstance = v;
-                if (_controlToValidateInstance) {
+                if (_controlToValidateInstance)
+                {
                     _controlToValidate = _controlToValidateInstance.id;
                 }
             }
@@ -144,29 +177,37 @@ var Validator = function (_props) {
     });
 
     let _beforeAttach = this.beforeAttach;
-    this.beforeAttach = function (e) {
-        if (e.target.id == this.domID) {
+    this.beforeAttach = function (e)
+    {
+        if (e.target.id == this.domID)
+        {
 
             if (typeof _beforeAttach == 'function')
                 _beforeAttach.apply(this, arguments);
 
-            if (_props.controlToValidate && !this.getBindingExpression("controlToValidate")) {
+            if (_props.controlToValidate && !this.getBindingExpression("controlToValidate"))
+            {
                 this.controlToValidate = _props.controlToValidate;
             }
-            if (_props.errorMessage && !this.getBindingExpression("errorMessage")) {
+            if (_props.errorMessage && !this.getBindingExpression("errorMessage"))
+            {
                 this.errorMessage = _props.errorMessage;
             }
 
-            if (_props.initialValue && !this.getBindingExpression("initialValue")) {
+            if (_props.initialValue && !this.getBindingExpression("initialValue"))
+            {
                 this.label = _initialValue = _props.initialValue;
             }
-            if (_props.setFocusOnError && !this.getBindingExpression("setFocusOnError")) {
+            if (_props.setFocusOnError && !this.getBindingExpression("setFocusOnError"))
+            {
                 this.setFocusOnError = _props.setFocusOnError;
             }
-            if (_props.validationGroup && !this.getBindingExpression("validationGroup")) {
+            if (_props.validationGroup && !this.getBindingExpression("validationGroup"))
+            {
                 this.validationGroup = _props.validationGroup;
-            }            
-            if (_props.required && !this.getBindingExpression("required")) {
+            }
+            if (_props.required && !this.getBindingExpression("required"))
+            {
                 this.required = _props.required;
             }
         }
@@ -195,7 +236,8 @@ var Validator = function (_props) {
 
     let r = Label.call(this, _props);
 
-    this.destruct = function (mode = 1) {
+    this.destruct = function (mode = 1)
+    {
         if (this.$el)
             mode == 1 ? this.$el.remove() : this.$el.detach();
         _self.attached = false;
@@ -205,6 +247,7 @@ var Validator = function (_props) {
     return r;
 };
 Validator.prototype.ctor = 'Validator';
-export {
+export
+{
     Validator
 };
