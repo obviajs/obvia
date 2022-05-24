5b4e038ec7640c9dac9a8a42f8795fc2da3969b9
Edra Fishta <edra.fishta@kreatx.al>
2022-05-24 09:55:35 +0200

    MultiUpload Fixes

--- components/MultiUpload.js
+++ components/MultiUpload.js
@@ -5,90 +5,107 @@
  */
 import { Container } from "/obvia/components/Container.js";
 import { ObjectUtils } from "/obvia/lib/ObjectUtils.js";
-var MultiUpload = function (_props) {
+import { DependencyContainer } from "/obvia/lib/DependencyContainer.js";
+import { Repeater } from "/obvia/components/Repeater/Repeater.js";
+import { UploadEx } from "/obvia/components/UploadEx.js";
+import { ContainerType } from "/obvia/components/base/ContainerType.js";
+import { BgStyle } from "/obvia/components/base/BgStyle.js";
+import { Label, LabelType } from "/obvia/components/Label.js";
+
+import { ProgressBar, ProgressBarStyle } from "/obvia/components/ProgressBar/ProgressBar.js";
+import { FormEventType } from "/obvia/components/Form/FormEventType.js";
+
+var MultiUpload = function (_props)
+{
     let _self = this;
     let _cmps, _lblDrop, _dropContainer, _listRepeater, _progressRow, _progressBar;
     let _dataProvider;
 
     Object.defineProperty(this, "dataProvider", {
-        get: function dataProvider() {
+        get: function dataProvider()
+        {
             return _dataProvider;
         },
-        set: function dataProvider(v) {
-            if (_dataProvider != v) {
+        set: function dataProvider(v)
+        {
+            if (_dataProvider != v)
+            {
                 _dataProvider = v;
             }
         }
     });
 
-    let fnContainerDelayInit = function () {
+    let fnContainerDelayInit = function ()
+    {
         _cmps = [{
-                ctor: Repeater,
-                props: {
-                    id: "listRepeater",
-                    rendering: {
-                        direction: "vertical",
-                        separator: false
-                    },
-                    dataProvider: _dataProvider,
-                    components: [{
-                        ctor: UploadEx,
-                        props: {
-                            id: "upload",
-                            change: _upload_change,
-                            form: _form,
-                            value: "{currentItem}",
-                            multiple: false,
-                            showProgress: false
-                        }
-                    }]
-                }
-            },
-            {
-                ctor: Container,
-                props: {
-                    id: "dropContainer",
-                    type: ContainerType.NONE,
-                    width: "100%",
-                    height: 50,
-                    classes: ["rounded-lg", "border"],
-                    components: [{
-                        ctor: Label,
-                        props: {
-                            id: "infoLabel",
-                            label: 'Drag and Drop File or Click Me',
-                            classes: ["text-center"],
-                            labelType: LabelType.p
-                        }
-                    }]
-                }
-            },
-            {
-                ctor: Container,
-                props: {
-                    id: "progressRow",
-                    type: ContainerType.NONE,
-                    classes: ["d-none", "progress"],
-                    height: 5,
-                    components: [{
-                        ctor: ProgressBar,
-                        props: {
-                            id: "progressbar",
-                            valueNow: 0,
-                            valueMin: 0,
-                            valueMax: 100,
-                            width: "100%",
-                            height: "100%",
-                            classes: [BgStyle.BG_INFO, ProgressBarStyle.PROGRESS, ProgressBarStyle.PROGRESS_ANIMATED, ProgressBarStyle.PROGRESS_STRIPED]
-                        }
-                    }]
-                }
+            ctor: Repeater,
+            props: {
+                id: "listRepeater",
+                rendering: {
+                    direction: "vertical",
+                    separator: false
+                },
+                dataProvider: _dataProvider,
+                components: [{
+                    ctor: UploadEx,
+                    props: {
+                        id: "upload",
+                        change: _upload_change,
+                        form: _form,
+                        value: "{currentItem}",
+                        multiple: false,
+                        showProgress: false
+                    }
+                }]
             }
+        },
+        {
+            ctor: Container,
+            props: {
+                id: "dropContainer",
+                type: ContainerType.NONE,
+                width: "100%",
+                height: 50,
+                classes: ["rounded-lg", "border"],
+                components: [{
+                    ctor: Label,
+                    props: {
+                        id: "infoLabel",
+                        label: 'Drag and Drop File or Click Me',
+                        classes: ["text-center"],
+                        labelType: LabelType.p
+                    }
+                }]
+            }
+        },
+        {
+            ctor: Container,
+            props: {
+                id: "progressRow",
+                type: ContainerType.NONE,
+                classes: ["d-none", "progress"],
+                height: 5,
+                components: [{
+                    ctor: ProgressBar,
+                    props: {
+                        id: "progressbar",
+                        valueNow: 0,
+                        valueMin: 0,
+                        valueMax: 100,
+                        width: "100%",
+                        height: "100%",
+                        classes: [BgStyle.BG_INFO, ProgressBarStyle.PROGRESS, ProgressBarStyle.PROGRESS_ANIMATED, ProgressBarStyle.PROGRESS_STRIPED]
+                    }
+                }]
+            }
+        }
         ];
     };
 
-    this.beforeAttach = function (e) {
-        if (e.target.id == this.domID) {
+    this.beforeAttach = function (e)
+    {
+        if (e.target.id == this.domID)
+        {
             _lblDrop = this.dropContainer.infoLabel;
             _dropContainer = this.dropContainer;
             _listRepeater = this.listRepeater;
@@ -100,8 +117,10 @@ var MultiUpload = function (_props) {
         }
     };
 
-    this.afterAttach = function (e) {
-        if (e.target.id == this.domID) {
+    this.afterAttach = function (e)
+    {
+        if (e.target.id == this.domID)
+        {
             $("html").on("dragover", _htmlDragOverHandler);
             $("html").on("drop", _htmlDropHandler);
             $("html").on("dragleave", _htmlDragLeaveHandler);
@@ -114,62 +133,76 @@ var MultiUpload = function (_props) {
 
     //TODO: on dp change set binding for show/hide trash icon
 
-    let _htmlDragOverHandler = function (e) {
+    let _htmlDragOverHandler = function (e)
+    {
         let t = e.originalEvent.dataTransfer.types;
-        if (t.includes("Files")) {
+        if (t.includes("Files"))
+        {
             e.preventDefault();
             e.stopPropagation();
             _lblDrop.label = "Drag here";
         }
     };
 
-    let _htmlDropHandler = function (e) {
+    let _htmlDropHandler = function (e)
+    {
         let t = e.originalEvent.dataTransfer.types;
-        if (t.includes("Files")) {
+        if (t.includes("Files"))
+        {
             e.preventDefault();
             e.stopPropagation();
             _lblDrop.label = "Drag and Drop File or Click Me";
         }
     };
 
-    let _htmlDragLeaveHandler = function (e) {
+    let _htmlDragLeaveHandler = function (e)
+    {
         let t = e.originalEvent.dataTransfer.types;
-        if (t.includes("Files")) {
+        if (t.includes("Files"))
+        {
             _lblDrop.label = "Drag and Drop File or Click Me";
         }
     };
 
-    let _dragEnterHandler = function (e) {
+    let _dragEnterHandler = function (e)
+    {
         e.preventDefault();
         e.stopPropagation();
         _lblDrop.label = "Drop";
     };
 
-    let _dragOverHandler = function (e) {
+    let _dragOverHandler = function (e)
+    {
         e.preventDefault();
         e.stopPropagation();
         _lblDrop.label = "Drop";
     };
 
-    let _dropHandler = function (e) {
+    let _dropHandler = function (e)
+    {
         e.preventDefault();
         e.stopPropagation();
         _lblDrop.label = "Drag and Drop File or Click Me";
 
-        for (let n = 0; n < e.originalEvent.dataTransfer.files.length; n++) {
+        for (let n = 0; n < e.originalEvent.dataTransfer.files.length; n++)
+        {
             let len = _listRepeater["upload"] ? _listRepeater["upload"].length : 0;
             let allUsed = true;
             let i, rowUpl;
-            for (i = 0; i < len && allUsed; i++) {
+            for (i = 0; i < len && allUsed; i++)
+            {
                 rowUpl = _listRepeater["upload"][i];
-                if (rowUpl.upload.files.length == 0 || !("size" in rowUpl.upload.files[0])) {
+                if (rowUpl.upload.files.length == 0 || !("size" in rowUpl.upload.files[0]))
+                {
                     allUsed = false;
                     break;
                 }
             }
-            if (allUsed) {
+            if (allUsed)
+            {
                 _dataProvider.push([e.originalEvent.dataTransfer.files[n]]);
-            } else {
+            } else
+            {
                 //_dataProvider[i] 
                 rowUpl.value = e.originalEvent.dataTransfer.files[n];
             }
@@ -187,13 +220,16 @@ var MultiUpload = function (_props) {
         uploadData(fd);
         */
     };
-    let _dragLeaveHandler = function (e) {
+    let _dragLeaveHandler = function (e)
+    {
         _lblDrop.label = "Drag and Drop File or Click Me";
     };
 
 
-    let _clickHandler = function (e) {
-        let fnUplRowAdded = function () {
+    let _clickHandler = function (e)
+    {
+        let fnUplRowAdded = function ()
+        {
             _listRepeater.off("onRowAdd", fnUplRowAdded);
             let len = _listRepeater["upload"].length;
             let rowUpl = _listRepeater["upload"][len - 1];
@@ -202,27 +238,32 @@ var MultiUpload = function (_props) {
 
         let len = _listRepeater["upload"] ? _listRepeater["upload"].length : 0;
         let allUsed = true;
-        for (let i = 0; i < len && allUsed; i++) {
+        for (let i = 0; i < len && allUsed; i++)
+        {
             let rowUpl = _listRepeater["upload"][i];
-            if (rowUpl.upload.files.length == 0 || !("size" in rowUpl.upload.files[0])) {
+            if (rowUpl.upload.files.length == 0 || !("size" in rowUpl.upload.files[0]))
+            {
                 allUsed = false;
                 rowUpl.upload.fileDialog();
                 break;
             }
         }
-        if (allUsed) {
+        if (allUsed)
+        {
             _listRepeater.on("onRowAdd", fnUplRowAdded);
             _listRepeater.dataProvider.push({});
         }
     };
 
-    let _upload_change = function (e) {
+    let _upload_change = function (e)
+    {
         console.log("UPL CHANGE: ", arguments);
         let last = _listRepeater.dataProvider.last();
         //last = Array.fromIterator(this.value);
     };
 
-    this.ajaxUpload = function () {
+    this.ajaxUpload = function ()
+    {
         _form.off(FormEventType.POST_ERROR, _ajaxUpload_error);
         _form.off(FormEventType.POST_SUCCESS, _ajaxUpload_success);
         _form.off(FormEventType.POST_PROGRESS, _ajaxUpload_progress);
@@ -236,33 +277,39 @@ var MultiUpload = function (_props) {
         _form.on(FormEventType.POST_STARTED, _ajaxUpload_started);
         let len = _listRepeater["upload"] ? _listRepeater["upload"].length : 0;
         let rp = new Array(len);
-        for (let i = 0; i < len; i++) {
+        for (let i = 0; i < len; i++)
+        {
             let rowUpl = _listRepeater["upload"][i];
             rp[i] = rowUpl.ajaxUpload(i == len - 1 ? false : true);
         }
         return Promise.all(rp);
     };
 
-    let _ajaxUpload_error = function (e, jqXHR, textStatus, errorThrown) {
+    let _ajaxUpload_error = function (e, jqXHR, textStatus, errorThrown)
+    {
         setTimeout(_ajaxUpload_complete, 500);
     };
 
-    let _ajaxUpload_success = function (e, data, textStatus, jqXHR) {
+    let _ajaxUpload_success = function (e, data, textStatus, jqXHR)
+    {
         setTimeout(_ajaxUpload_complete, 500);
     };
 
-    let _ajaxUpload_progress = function (e, xhrProgressEvt) {
+    let _ajaxUpload_progress = function (e, xhrProgressEvt)
+    {
         _progressBar.valueNow = xhrProgressEvt.percentage;
     };
 
-    let _ajaxUpload_started = function (e) {
+    let _ajaxUpload_started = function (e)
+    {
         let classes = _progressRow.classes.slice(0);
         classes.splice(classes.indexOf("d-none"), 1);
         _progressBar.valueNow = 0;
         _progressRow.classes = classes;
     };
 
-    let _ajaxUpload_complete = function (e) {
+    let _ajaxUpload_complete = function (e)
+    {
         let classes = _progressRow.classes.slice(0);
         classes.pushUnique("d-none");
         _progressRow.classes = classes;
@@ -290,7 +337,8 @@ var MultiUpload = function (_props) {
         this.showBtnRemove = _props.showBtnRemove;
 
     let _destruct = this.destruct;
-    this.destruct = function (mode = 1) {
+    this.destruct = function (mode = 1)
+    {
         $("html").off("dragover", _htmlDragOverHandler);
         $("html").off("drop", _htmlDropHandler);
         $("html").off("dragleave", _htmlDragLeaveHandler);
@@ -304,6 +352,9 @@ var MultiUpload = function (_props) {
     };
 };
 MultiUpload.prototype.ctor = 'MultiUpload';
-export {
+DependencyContainer.getInstance().register("MultiUpload", MultiUpload, DependencyContainer.simpleResolve);
+
+export
+{
     MultiUpload
 };
--- components/base/BgStyle.js
+++ components/base/BgStyle.js
@@ -1,7 +1,7 @@
-var BgStyle =
+export var BgStyle =
 {
-    "BG_SUCCESS":"bg-success",
-    "BG_INFO":"bg-info",
-    "BG_WARNING":"bg-warning",
-    "BG_DANGER":"bg-danger"
+    "BG_SUCCESS": "bg-success",
+    "BG_INFO": "bg-info",
+    "BG_WARNING": "bg-warning",
+    "BG_DANGER": "bg-danger"
 }
