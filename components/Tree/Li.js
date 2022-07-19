e96cd2651e156def0d32719b9c9c5ef5e855c6d7
MyftariA <armando.myftari@kreatx.al>
2022-07-19 17:18:20 +0200

    register Li dependency for usage

--- components/Tree/Li.js
+++ components/Tree/Li.js
@@ -1,17 +1,23 @@
-import {Container} from "/obvia/components/Container.js";
+import { Container } from "/obvia/components/Container.js";
 import { ObjectUtils } from "/obvia/lib/ObjectUtils.js";
+import { DependencyContainer } from "/obvia/lib/DependencyContainer.js";
 
-var Li = function (_props) {
+var Li = function (_props)
+{
     let _self = this, _label, _value;
-    
+
     Object.defineProperty(this, "label", {
-        get: function label() {
+        get: function label()
+        {
             return _label;
         },
-        set: function label(v) {
-            if (_label != v) {
+        set: function label(v)
+        {
+            if (_label != v)
+            {
                 _label = v;
-                if (this.$el) {
+                if (this.$el)
+                {
                     v = $(`<div>${v}</div>`).get(0).innerText;
                     let last = this.$el.children().last();
                     if (last && last.length > 0)
@@ -29,26 +35,27 @@ var Li = function (_props) {
         configurable: true
     });
 
-    Object.defineProperty(this, "value", 
-    {
-        get: function value() 
+    Object.defineProperty(this, "value",
         {
-            return _value;
-        },
-        set: function value(v) 
-        {
-            if(_value != v)
+            get: function value() 
             {
-                _value = v;
-                if(this.$el)
-                    this.$el.attr('value', v);
-            }
-        },
-        enumerable: true
-    });
+                return _value;
+            },
+            set: function value(v) 
+            {
+                if (_value != v)
+                {
+                    _value = v;
+                    if (this.$el)
+                        this.$el.attr('value', v);
+                }
+            },
+            enumerable: true
+        });
 
-    this.template = function () {
-        return "<li id='" + this.domID + "' value='"+_value+"'></li>";
+    this.template = function ()
+    {
+        return "<li id='" + this.domID + "' value='" + _value + "'></li>";
     };
 
     let _defaultParams = {
@@ -58,15 +65,17 @@ var Li = function (_props) {
     };
     ObjectUtils.fromDefault(_defaultParams, _props);
     //_props = ObjectUtils.extend(false, false, _defaultParams, _props);
-   
-    let r = Container.call(this, _props); 
-   
+
+    let r = Container.call(this, _props);
+
     _label = _props.label;
     _value = _props.value;
     return r;
 };
 //component prototype
 Li.prototype.ctor = 'Li';
-export {
+DependencyContainer.getInstance().register("Li", Li, DependencyContainer.simpleResolve);
+export
+{
     Li
 };
