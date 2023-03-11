06d600f0302ef568813d9ccdb73a93136aed8ea0
MyftariA <armando.myftari@kreatx.al>
2023-03-12 00:41:05 +0100

    Fixed finding index for validator delete

--- components/Validation/ValidationManager.js
+++ components/Validation/ValidationManager.js
@@ -1,36 +1,49 @@
 import { EventDispatcher } from "/obvia/lib/EventDispatcher.js";
-var ValidationManager = function () {
+var ValidationManager = function ()
+{
     let _self, _validators = [];
     this.$el = $(this);
 
-    this.getGroupValidators = function(validationGroup = null){
-        if (validationGroup) {
-            let groups = _validators.groupReduce((currentValue, index) => {
+    this.getGroupValidators = function (validationGroup = null)
+    {
+        if (validationGroup)
+        {
+            let groups = _validators.groupReduce((currentValue, index) =>
+            {
                 return currentValue.validationGroup;
             });
             return groups[validationGroup];
         }
     };
-        
-    this.validate = function (validationGroup = null) {      
+
+    this.validate = function (validationGroup = null)
+    {
         let promises = [];
-        if (validationGroup) {
-            let groups = _validators.groupReduce((currentValue, index) => { 
+        if (validationGroup)
+        {
+            let groups = _validators.groupReduce((currentValue, index) =>
+            {
                 return currentValue.validationGroup;
             });
             let av = groups[validationGroup];
-            if (av) {
+            if (av)
+            {
                 let len = av.length;
-                for (let i = 0; i < len; i++) {
-                    if (av[i]) { 
+                for (let i = 0; i < len; i++)
+                {
+                    if (av[i])
+                    {
                         promises.push(av[i].validate());
-                    }                    
+                    }
                 }
             }
-        } else {
+        } else
+        {
             let len = _validators.length;
-            for (let i = 0; i < len; i++) { 
-                if (_validators[i]) { 
+            for (let i = 0; i < len; i++)
+            {
+                if (_validators[i])
+                {
                     promises.push(_validators[i].validate());
                 }
             }
@@ -50,7 +63,7 @@ var ValidationManager = function () {
         } else
         {
             av = _validators;
-        }        
+        }
         if (av)
         {
             let len = av.length;
@@ -63,25 +76,30 @@ var ValidationManager = function () {
             }
         }
     };
-    this.add = function (validator) {
+    this.add = function (validator)
+    {
         _validators.push(validator);
     };
-    
-    this.remove = function (validator) {
-        let ind = _validators.indexOf(validator);
-        if (ind > -1) {
+
+    this.remove = function (validator)
+    {
+        let ind = _validators.findIndex(self => self.id == validator.id)
+        if (ind > -1)
+        {
             _validators.splice(ind, 1);
         }
     };
 };
 ValidationManager.ctor = "ValidationManager";
 ValidationManager.instance = null;
-ValidationManager.getInstance = function () {
+ValidationManager.getInstance = function ()
+{
     if (!ValidationManager.instance)
         ValidationManager.instance = new ValidationManager();
     return ValidationManager.instance;
-};     
+};
 ValidationManager.prototype = Object.create(EventDispatcher.prototype);
-export {
+export
+{
     ValidationManager
 };
