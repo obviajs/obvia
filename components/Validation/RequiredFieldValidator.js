234fd4d9694f6c1a67b27cb9c5cc11162d06cad7
xhena <raglixhena.arizi@kreatx.al>
2024-05-10 14:39:00 +0200

    Minor fix for string validation.

--- components/Validation/RequiredFieldValidator.js
+++ components/Validation/RequiredFieldValidator.js
@@ -18,7 +18,8 @@ var RequiredFieldValidator = function (_props)
         if (_controlToValidateInstance)
         {
             let value = _controlToValidateInstance.valueProp ? _controlToValidateInstance[_controlToValidateInstance.valueProp] : _controlToValidateInstance.value;
-            if (!_self.enabled || (value != null && value !== "" && !Array.isArray(value) || (Array.isArray(value) && value.length > 0)))
+            const isValidStr = typeof value === "string" && value.trim() !== "";
+            if (!_self.enabled || (value != null && isValidStr && !Array.isArray(value) || (Array.isArray(value) && value.length > 0)))
             {
                 _self.isValid = true;
             } else
