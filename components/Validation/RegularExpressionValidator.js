55b028b41dab555c7fa084bae4554e1aac52824c
Obvia <66308429+obviajs@users.noreply.github.com>
2025-11-30 18:32:40 +0100

    Refactor validation logic in RegularExpressionValidator

--- components/Validation/RegularExpressionValidator.js
+++ components/Validation/RegularExpressionValidator.js
@@ -17,7 +17,7 @@ var RegularExpressionValidator = function (_props)
         let _controlToValidateInstance = _self.controlToValidateInstance;
         if (_controlToValidateInstance)
         {
-            if ((!_self.required && (_controlToValidateInstance.value == null || _controlToValidateInstance.value == "")) || (!_self.enabled && _controlToValidateInstance.value == "") || (_controlToValidateInstance.value && _validationExpressionCompiled.test(_controlToValidateInstance.value)))
+            if (!_self.enabled || (_controlToValidateInstance.value && _validationExpressionCompiled.test(_controlToValidateInstance.value)))
             {
                 _self.isValid = true;
             } else
